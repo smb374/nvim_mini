2738e048e150ff4be19365d2d5e7dd75b0edd592
Po-Yeh Chen <poyehchen@cs.nycu.edu.tw>
2025-03-13 13:15:33 -0400

    Add ocaml stuff.

---
+++ lua/plugins/lang/ocaml.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "ocaml",
+        "ocaml_interface",
+        "ocamllex",
+      })
+    end,
+  },
+  {
+    "nvim-lspconfig",
+    opts = function(_, opts)
+      vim.list_extend(opts.external_servers, { "ocamllsp" })
+    end,
+  },
+}
